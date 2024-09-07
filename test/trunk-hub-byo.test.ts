f098cc5e20374087dc525cc3825e1aa2e8b2ac04
Joe Sustaric <joe8307@gmail.com>
2024-09-07 10:59:51 +1000

    cdk init app --language typescript

---
+++ test/trunk-hub-byo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrunkHubByo from '../lib/trunk-hub-byo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trunk-hub-byo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrunkHubByo.TrunkHubByoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
