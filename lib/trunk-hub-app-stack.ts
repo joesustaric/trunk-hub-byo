448644267838b60c08c80dd4f37166632865be8f
Joe Sustaric <joe8307@gmail.com>
2024-09-15 21:53:10 +1000

    sorting lines

--- lib/trunk-hub-app-stack.ts
+++ lib/trunk-hub-app-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 interface TrunkHubAppStackProps extends cdk.StackProps {
     vpcStackName: string;
@@ -15,20 +15,20 @@ export class TrunkHubAppStack extends cdk.Stack {
 
         //Import values needed from VPC Stack Outputs
         //TODO: or pass these in?
-        const vpcId = cdk.Fn.importValue(`${props.vpcStackName}:VpcId`);
-        const publicSubnet1 = cdk.Fn.importValue(`${props.vpcStackName}:PublicSubnet1`);
-        const publicSubnet2 = cdk.Fn.importValue(`${props.vpcStackName}:PublicSubnet2`);
-        const privateSubnet1 = cdk.Fn.importValue(`${props.vpcStackName}:PrivateSubnet1`);
-        const privateSubnet2 = cdk.Fn.importValue(`${props.vpcStackName}:PrivateSubnet2`);
         const az1 = cdk.Fn.importValue(`${props.vpcStackName}:AvailabilityZone1`);
         const az2 = cdk.Fn.importValue(`${props.vpcStackName}:AvailabilityZone2`);
+        const privateSubnet1 = cdk.Fn.importValue(`${props.vpcStackName}:PrivateSubnet1`);
+        const privateSubnet2 = cdk.Fn.importValue(`${props.vpcStackName}:PrivateSubnet2`);
+        const publicSubnet1 = cdk.Fn.importValue(`${props.vpcStackName}:PublicSubnet1`);
+        const publicSubnet2 = cdk.Fn.importValue(`${props.vpcStackName}:PublicSubnet2`);
+        const vpcId = cdk.Fn.importValue(`${props.vpcStackName}:VpcId`);
 
         // Import the VPC using the imported VPC ID and availability zones
         const vpc = ec2.Vpc.fromVpcAttributes(this, 'ImportedVPC', {
-            vpcId: vpcId,
             availabilityZones: [az1, az2],
+            privateSubnetIds: [privateSubnet1, privateSubnet2],
             publicSubnetIds: [publicSubnet1, publicSubnet2],
-            privateSubnetIds: [privateSubnet1, privateSubnet2]
+            vpcId: vpcId,
         });
 
         // Create an Application Load Balancer
@@ -68,17 +68,21 @@ export class TrunkHubAppStack extends cdk.Stack {
         const ec2InstanceConnectRole = new iam.Role(this, 'EC2InstanceConnectRole', {
             assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
             managedPolicies: [
-            iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
-            iam.ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceConnect')
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceConnect')
             ],
         });
 
         const securityGroup = new ec2.SecurityGroup(this, 'InstanceSecurityGroup', {
-            vpc,
             allowAllOutbound: true,
+            vpc,
         });
-        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access');
-        securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow http access');
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH access'
+        );
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow http access'
+        );
 
         // User data script to set up a web server
         const userData = ec2.UserData.forLinux();
@@ -86,13 +90,20 @@ export class TrunkHubAppStack extends cdk.Stack {
             'yum update -y',
             'yum install -y httpd',
             'systemctl enable httpd',
-            'systemctl start httpd'
+            'systemctl start httpd',
         );
 
         // Create an Auto Scaling group
         const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'AutoScalingGroup', {
+            blockDevices: [{
+                deviceName: '/dev/xvda', // Root volume
+                volume: autoscaling.BlockDeviceVolume.ebs(8, {
+                    volumeType: autoscaling.EbsDeviceVolumeType.GP3,
+                    encrypted: true,
+                    deleteOnTermination: true,
+                }),
+            },],
             desiredCapacity: 2,
-
             instanceType: new ec2.InstanceType('t4g.micro'),
             machineImage: new ec2.AmazonLinuxImage({
                 cpuType: ec2.AmazonLinuxCpuType.ARM_64,
@@ -104,31 +115,20 @@ export class TrunkHubAppStack extends cdk.Stack {
             maxCapacity: 2,
             minCapacity: 2,
             role: ec2InstanceConnectRole,
+            securityGroup: securityGroup,
             userData: userData,
             vpc,
             vpcSubnets: {
-                subnets: vpc.publicSubnets,
+                subnets: vpc.privateSubnets,
             },
-            blockDevices: [
-                {
-                    deviceName: '/dev/xvda', // Root volume
-                    volume: autoscaling.BlockDeviceVolume.ebs(8, { // 8 GiB gp3 volume
-                        volumeType: autoscaling.EbsDeviceVolumeType.GP3,
-                        encrypted: true,
-                        deleteOnTermination: true,
-                    }),
-                },
-            ],
         });
-
-        // Attach the security group to the Auto Scaling Group
-        autoScalingGroup.addSecurityGroup(securityGroup);
 
         // // Attach the Auto Scaling group to the ALB
         const listener = alb.addListener('Listener', {
             port: 80,
             open: true,
         });
+        
         listener.addTargets('Targets', {
             port: 80,
             targets: [autoScalingGroup],
--- lib/trunk-hub-vpc-stack.ts
+++ lib/trunk-hub-vpc-stack.ts
@@ -14,6 +14,7 @@ export class TrunkHubVPCStack extends cdk.Stack {
     const availabilityZones = cdk.Stack.of(this).availabilityZones.slice(0, 2);
 
     const vpc = new ec2.Vpc(this, 'trunk-hub-vpc', {
+      availabilityZones: availabilityZones,
       ipAddresses: ec2.IpAddresses.cidr(props.vpcCidr),
       subnetConfiguration: [
         {
@@ -27,53 +28,52 @@ export class TrunkHubVPCStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
         },
       ],
-      availabilityZones: availabilityZones,
     });
 
       // Add EC2 Instance Connect endpoint using EC2 service endpoint
       vpc.addInterfaceEndpoint('Ec2InstanceConnectEndpoint', {
+        privateDnsEnabled: true,
         service: ec2.InterfaceVpcEndpointAwsService.EC2,
         subnets: {
           availabilityZones: availabilityZones,
           subnets: [
-            vpc.publicSubnets[0], // First public subnet
-            vpc.privateSubnets[1] // First private subnet
+            vpc.publicSubnets[0],
+            vpc.privateSubnets[1]
           ],
         },
-        privateDnsEnabled: true,
       });
 
     // Output the availability zones as individual parameters
     availabilityZones.forEach((az, index) => {
       new cdk.CfnOutput(this, `AvailabilityZone${index + 1}`, {
-        value: az,
         description: `Availability Zone ${index + 1}`,
         exportName: `${id}:AvailabilityZone${index + 1}`,
+        value: az,
       });
     });
 
     // Output the VPC ID
     new cdk.CfnOutput(this, 'VpcId', {
-      value: vpc.vpcId,
       description: 'ID of the VPC',
-      exportName: `${id}:VpcId`
+      exportName: `${id}:VpcId`,
+      value: vpc.vpcId,
     });
 
     // Output the ARNs of the public subnets
     vpc.publicSubnets.forEach((subnet, index) => {
       new cdk.CfnOutput(this, `PublicSubnet${index + 1}`, {
-        value: subnet.subnetId,
         description: `ID of public subnet ${index + 1}`,
-        exportName: `${id}:PublicSubnet${index + 1}`
+        exportName: `${id}:PublicSubnet${index + 1}`,
+        value: subnet.subnetId,
       });
     });
 
     // Output the ARNs of the private subnets
     vpc.privateSubnets.forEach((subnet, index) => {
       new cdk.CfnOutput(this, `PrivateSubnet${index + 1}`, {
-        value: subnet.subnetId,
         description: `ID of private subnet ${index + 1}`,
-        exportName: `${id}:PrivateSubnet${index + 1}`
+        exportName: `${id}:PrivateSubnet${index + 1}`,
+        value: subnet.subnetId,
       });
     });
   }
