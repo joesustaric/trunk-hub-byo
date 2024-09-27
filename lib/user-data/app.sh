ae383d5813872222e210b3091255395df559b43b
Joe Sustaric <joe8307@gmail.com>
2024-09-27 15:07:54 +1000

    adding some todos

--- lib/user-data/app.sh
+++ lib/user-data/app.sh
@@ -15,14 +15,15 @@ sudo su git
 cd /home/git
 
 # Set up SSH directory and authorized keys
+# TODO: Fix this so that you can add keys somewhere else.
 echo "Setting up SSH directory and authorized keys..."
 mkdir -p .ssh && chmod 700 .ssh
 sudo chown -R git:git /home/git/.ssh
 sudo runuser -l git -c 'touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys'
 sudo runuser -l git -c 'echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3OuNRLfCK3upvG6JKmDAlnsl6x4bxkCnKQbrIt7+uk joe@emaill.com" >> ~/.ssh/authorized_keys'
 
-
-# Set up a bare Git repository
+# Set up a bare Git repository for testing
+# TODO: Fix the default branch name.
 echo "Setting up a bare Git repository..."
 sudo runuser -l git -c 'mkdir -p /srv/git/trunk-hub-test.git'
 sudo runuser -l git -c 'cd /srv/git/trunk-hub-test.git && git init --bare --initial-branch=trunk'
@@ -57,4 +58,3 @@ chown root:root /etc/ssh/ssh_host_ed25519_key.pub
 systemctl restart sshd
 
 echo "User data script completed successfully."
-
